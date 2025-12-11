2e33350e17d148d9add1f77a32bfd9e5b746af2a
aidenjajo <ajajo1344@sdsu.edu>
2025-12-10 21:21:30 -0800

    Update fraud_detection_smote.py

--- 07-imbalanced-data/fraud_detection_smote.py
+++ 07-imbalanced-data/fraud_detection_smote.py
@@ -2,8 +2,8 @@
 Fraud Detection with Imbalanced Data Handling
 CS577-01: Principles and Techniques of Data Science
 San Diego State University
-Author: Aiden
-Date: October 2024
+Author: Aiden Jajo
+Spring 2025
 """
 
 # ============================================================================
